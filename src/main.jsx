0bba44cd455241af82da7e2ceae8fc220c26899a
Rahul kumar gupta <rahulkrgupta0407@gmail.com>
2024-11-25 22:48:00 +0530

    connect_scss

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./styles/app.scss"
 import { Bounce, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { store, persistor } from "./redux/store.js";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
+
 // File Imports
 // import React from "react";
 import { motion } from "framer-motion";
-import Hero from "../components/home/hero";
-=======
+
 // file imports
 import Hero from "../components/home/Hero";
->>>>>>> 39a1d95 (dashboard_update)
 import About from "../components/home/About";
 import Service from "../components/home/Service";
 import Team from "../components/home/Team";
