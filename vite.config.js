7a0f8e0f49ab0245d30c20e688cba52afd357e33
NAMAN__AMRIT_ARJUN <62653226+6829nkhpas@users.noreply.github.com>
2025-04-25 15:47:41 +0530

    run build

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        secure: false,
-      },
-    },
-    allowedHosts: "*",
-  },
+  
   plugins: [react()],
 });
